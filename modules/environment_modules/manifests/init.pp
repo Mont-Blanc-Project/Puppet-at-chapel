3cb20abb90cd0314e0684800bddee6e8549cd11c
Josep Oriol Vilarrubi <josep.vilarrubibarri@bsc.es>
2015-11-18 16:05:43 +0100

    more refactoring

--- modules/environment_modules/manifests/init.pp
+++ modules/environment_modules/manifests/init.pp
@@ -1,16 +1,16 @@
-class modulees {
+class environment_modules {
 	package { "tclx8.4-dev":
 		ensure => latest,
 	}
 	file { '/etc/profile.d/modules.sh':
-		source => "puppet:///modules/modulees/modules.sh",
+		source => "puppet:///modules/$module_name/modules.sh",
 		ensure => file,
 		owner => 'root',
 		group => 'root',
 		mode => '644',
 	}
 	file { '/etc/skel/.bashrc':
-		source => 'puppet:///modules/modulees/bashrc',
+		source => "puppet:///modules/$module_name/bashrc",
 		ensure => file,
 		owner => 'root',
 		group => 'root',
